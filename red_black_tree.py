5da655004254f39b8839c97b66b90bfb5418c597
Pavel <157171975+BartaPavel09@users.noreply.github.com>
2026-04-11 12:04:36 +0300

    feat: added red-black tree implementation
    Introduce RBNode and RedBlackTree classes in red_black_tree.py. Implements a Nil sentinel node, left/right rotations, insert with insert_fixup to maintain red-black properties, and recursive search. Nodes store a key and an optional patient_name. Delete operations are not implemented.

---
+++ red_black_tree.py
@@ -0,0 +1,113 @@
+class RBNode:
+    def __init__(self, key, patient_name=""):
+        self.key = key
+        self.patient_name = patient_name
+        self.col = "BLACK"
+        self.p = None
+        self.left = None
+        self.right = None
+
+class RedBlackTree:
+    def __init__(self):
+        self.Nil = RBNode(0)
+        self.Nil.col = "BLACK"
+        self.Nil.left = self.Nil
+        self.Nil.right = self.Nil
+        self.root = self.Nil
+
+    def is_nil(self, node):
+        return node == self.Nil
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.Nil:
+            y.left.p = x
+        y.p = x.p
+        if x.p == self.Nil:
+            self.root = y
+        elif x == x.p.left:
+            x.p.left = y
+        else:
+            x.p.right = y
+        y.left = x
+        x.p = y
+
+    def right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+        if x.right != self.Nil:
+            x.right.p = y
+        x.p = y.p
+        if y.p == self.Nil:
+            self.root = x
+        elif y == y.p.left:
+            y.p.left = x
+        else:
+            y.p.right = x
+        x.right = y
+        y.p = x
+
+    def insert(self, key, patient_name):
+        z = RBNode(key, patient_name)
+        
+        y = self.Nil
+        x = self.root
+        while not self.is_nil(x):
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+
+        z.p = y
+        if self.is_nil(y):
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+
+        z.left = self.Nil
+        z.right = self.Nil
+        z.col = "RED"
+        self.insert_fixup(z)
+
+    def insert_fixup(self, z):
+        while z.p.col == "RED":
+            if z.p == z.p.p.left:
+                y = z.p.p.right
+                if y.col == "RED":
+                    z.p.col = "BLACK"
+                    y.col = "BLACK"
+                    z.p.p.col = "RED"
+                    z = z.p.p
+                else:
+                    if z == z.p.right:
+                        z = z.p
+                        self.left_rotate(z)
+                    z.p.col = "BLACK"
+                    z.p.p.col = "RED"
+                    self.right_rotate(z.p.p)
+            else:
+                y = z.p.p.left
+                if y.col == "RED":
+                    z.p.col = "BLACK"
+                    y.col = "BLACK"
+                    z.p.p.col = "RED"
+                    z = z.p.p
+                else:
+                    if z == z.p.left:
+                        z = z.p
+                        self.right_rotate(z)
+                    z.p.col = "BLACK"
+                    z.p.p.col = "RED"
+                    self.left_rotate(z.p.p)
+        self.root.col = "BLACK"
+
+    def search(self, w, key):
+        if self.is_nil(w) or w.key == key:
+            return w
+        if key < w.key:
+            return self.search(w.left, key)
+        return self.search(w.right, key)
